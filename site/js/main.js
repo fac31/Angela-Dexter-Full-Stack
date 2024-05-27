24973c46c20cd664e337ded67dda865031d82bb0
Anxhela Berisha <aggelikhberisa@gmail.com>
2024-05-27 11:39:57 +0100

    CrimeDefinitions popup

--- site/js/main.js
+++ site/js/main.js
@@ -1,2 +1,14 @@
 // there HERE api key doesnt actually have to be hidden
 window.HERE_API_KEY = "2jVwbhCihNJP6YbnvpTcYMaUso_xB6YMZWm4UiOjBLQ";
+
+
+import { showCrimeDefinitions } from "./crimeDefinitions.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const viewDefinitionsBtn = document.getElementById("viewDefinitions");
+    if (viewDefinitionsBtn) {
+        viewDefinitionsBtn.addEventListener("click", () => {
+            showCrimeDefinitions();
+        });
+    }
+});
